8388f213887cc8499023569a99a7b8f5831f81a8
JuanYut <juanyut97@hotmail.com>
2026-08-12 10:05:07 -0600

    Inicio de proyecto, que suene

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <section id="center">
+      <img src="/favicon_colimoteclub.svg" alt="colimote.club" width={48} height={48} />
+      <h1>colimote.club</h1>
+    </section>
+  );
+}
+
+export default App;
